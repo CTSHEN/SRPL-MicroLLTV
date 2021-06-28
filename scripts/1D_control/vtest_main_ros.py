b5872def526c00fd59088795022713b7f1afb4d8
CTSHEN <eric810819@hotmail.com>
2021-06-28 19:17:14 +0800

    (doMPC with ROS) create ROS main file and model file

---
+++ scripts/1D_control/vtest_main_ros.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import rospy
+
+
+import do_mpc
+from VTest_MPC import VTest_mpc
+from vtest_model_ros import VTest_model
+from VTest_MPC import VTest_mpc
+from VTest_Simulator import VTest_simulator
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+# Constants
+N = 100
+
+model = VTest_model()
+mpc = VTest_mpc(model)
+simulator = VTest_simulator(model)
+
+# Set the initial state of simulator
+x0 = np.array([0, 0, 17.0])
+simulator.x0 = x0
+mpc.x0 = x0
+
+mpc.set_initial_guess()
+
+# Customizing Matplotlib
+mpl.rcParams['font.size'] = 18
+mpl.rcParams['lines.linewidth'] = 3
+mpl.rcParams['axes.grid'] = True
+
+# Setup graphics
+mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
+sim_graphics = do_mpc.graphics.Graphics(simulator.data)
+
+fig, ax = plt.subplots(5, sharex=True, figsize=(16,10))
+fig.align_ylabels()
+plt.ion()
+
+for g in [sim_graphics, mpc_graphics]:
+    # Plot r1, r2 and m on the first axis
+    g.add_line(var_type='_x', var_name='r_1', axis=ax[0])
+    g.add_line(var_type='_x', var_name='r_2', axis=ax[0])
+    g.add_line(var_type='_tvp', var_name='position_setpoint', axis=ax[0], linestyle='--')
+    g.add_line(var_type='_x', var_name='m', axis=ax[1])
+
+    # Plot control inputs
+    g.add_line(var_type='_u', var_name='Td', axis=ax[2])
+    g.add_line(var_type='_u', var_name='Tu', axis=ax[2])
+
+ax[0].set_title('Simulation Result')
+ax[0].set_ylabel('meter')
+ax[0].legend(mpc_graphics.result_lines['_x','r_1']
+     + mpc_graphics.result_lines['_x','r_2']+ mpc_graphics.result_lines['_tvp','position_setpoint']
+     , ['position', 'velocity','position_setpoint'], fontsize=10)
+ax[0].set_ylim([-1,1.5])
+ax[0].set_xlim([0,31])
+
+ax[1].set_title('Total Mass', fontsize =12)
+ax[1].set_ylabel('kg')
+ax[1].legend(mpc_graphics.result_lines['_x','m'],['total mass'], fontsize=10)
+ax[1].set_ylim([16.5,17.1])
+ax[1].set_xlim([0,31])
+
+ax[2].set_title('control_signal', fontsize =12)
+#ax[2].set_xlabel('time [s]')
+ax[2].legend(mpc_graphics.result_lines['_u','Td'] 
+    + mpc_graphics.result_lines['_u','Tu'],['Thrust Down','Thrust Up'], fontsize=10)
+ax[2].set_ylim([0,1])
+ax[2].set_xlim([0,31])
+
+
+_x = model.x
+# Running Simulator
+#while mpc._x['m'] >= 6.5: #(model.m_2 + model.m_1s):
+
+# some parameters for bang-bang
+kappa = 0.1
+ud_last = np.array([[0],[0]])
+ud = np.zeros((2,N))
+
+for k in range(N):
+#u0 = mpc.make_step(x0)
+#while mpc.data['_x','m',0] >= 6.5:
+    u0 = mpc.make_step(x0)
+    for cmd in range(2):
+        if ud_last[cmd,0] == 1:
+            if u0[cmd,0] >= (1-kappa)*1/2:
+                ud[cmd,k] = 1
+            else:
+                ud[cmd,k] = 0
+        elif ud_last[cmd,0] == 0:
+            if u0[cmd,0] >= (1+kappa)*1/2:
+                ud[cmd,k] = 1
+            else:
+                ud[cmd,k] = 0
+        else:
+            print('ERROR')
+            quit()
+        
+    ud_last = ud[0:2,k:k+1]
+    
+    x0 = simulator.make_step(ud[0:2,k:k+1])
+    #u0 = mpc.make_step(x0)
+
+    ax[3].set_title('outputDown', fontsize=10)
+    ax[3].plot(mpc.data['_time'],ud[0:1,0:k+1].transpose(),drawstyle='steps-post',color='orange')
+    ax[4].set_title('outputUp', fontsize=10)
+    ax[4].set_xlabel('time [s]')
+    ax[4].plot(mpc.data['_time'],ud[1:2,0:k+1].transpose(),drawstyle='steps-post',color='blue')
+
+    mpc_graphics.plot_results()
+    #sim_graphics.plot_results()
+    #mpc_graphics.reset_axes()
+    #sim_graphics.reset_axes()
+    plt.show()
+    plt.pause(0.01)
+
+input('Press ENTER to exit.')
---
+++ scripts/1D_control/vtest_model_ros.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import numpy as np
+import do_mpc
+from casadi import *
+
+def VTest_model():
+    # Obtain an instance of the do-mpc model class
+    # and select time discretization:
+    model_type = 'continuous' # either 'discrete' or 'continuous'
+    model = do_mpc.model.Model(model_type)
+
+    # Introduce new states, inputs and other variables to the model, e.g.:
+    r_1 = model.set_variable(var_type='_x', var_name='r_1', shape=(1,1))
+    r_2 = model.set_variable(var_type='_x', var_name='r_2', shape=(1,1))
+    m = model.set_variable(var_type='_x', var_name='m', shape=(1,1))
+
+    Td = model.set_variable(var_type='_u', var_name='Td',shape=(1,1))
+    Tu = model.set_variable(var_type='_u', var_name='Tu',shape=(1,1))
+
+    position_setpoint = model.set_variable(var_type='_tvp',var_name='position_setpoint')
+    velocity_setpoint = model.set_variable(var_type='_tvp',var_name='velocity_setpoint')
+    
+    # Some constant numbers
+    m_2 = 8 # Mass of m2
+    m_1s = 8.5 # Structure mass for m1
+    m_1f = 0.5 # Initial fuel mass
+    m_init = m_1s+m_1f+m_2
+    fuelFlowRate = 0.018
+    G = 9.81
+    
+    
+
+    # Set right-hand-side of ODE for all introduced states (_x).
+    # Names are inherited from the state definition.
+    r2_next = 15*(Td-Tu)/m + (2*m_2/m -1)*G
+    m_next = -fuelFlowRate*(Td+Tu)
+    model.set_rhs('r_1', r_2)
+    model.set_rhs('r_2', r2_next)
+    model.set_rhs('m', m_next)
+
+    # Setup model:
+    model.setup()
+
+    return model
