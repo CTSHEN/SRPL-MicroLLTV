13aa5712b5f9ac9317de119509643a06c6c4e836
CTSHEN <eric810819@hotmail.com>
2021-11-05 23:41:13 +0800

    just finish v0.2, still need some debugging.

--- src/one_dim_ocp.cpp
+++ src/one_dim_ocp.cpp
@@ -1,6 +1,7 @@
-//////////////////////////////////////////////////////////////
-//////////// 1-D Optimal control with ROS in MPC sheme////////
-//////////////////////////////////////////////////////////////
+/*************************************************************
+* 1-D Optimal control v0.2 
+
+*************************************************************/
 
 #include "psopt.h"
 
@@ -19,7 +20,166 @@
 #define DIS_X  1.5
 #define DIS_V  0
 
+#define DELTA_T 0.2
+#define KAPPA 0.2
+
 using namespace PSOPT;
+
+///////// Contorl loop class definition /////////
+class MainControlLoop
+{
+    public:
+        MainControlLoop(ros::NodeHandle *nh,
+            Prob &problemIn, Sol &solutionIn, Alg &algorithmIn)
+        {
+            last_control = 0.0;
+            rk4_control.resize(1,2);
+            rk4_time.resize(1,2);
+            rk4_states.resize(3,2);
+            init_states.resize(3,1);
+
+            problem = problemIn;
+            solution = solutionIn;
+            algorithm = algorithmIn;
+
+            Control_nh = nh;
+
+            //Publish control (on-off)
+            control_pub = nh->advertise<std_msgs::Float32>
+            ("control", 1);
+
+        }
+        
+        double curr_Time;
+        adouble curr_Time_a;
+        adouble& curr_Time_ref = curr_Time_a;
+        double stamp_now, stamp_next;
+        Prob problem;
+        Sol solution;
+        Alg algorithm;
+        ros::NodeHandle *Control_nh;       
+        
+
+        // vectors for rk4 propagate
+        MatrixXd rk4_control, rk4_time, rk4_states, rk4_param, init_states;
+        
+        
+
+        void GetNextControl()
+        {
+            curr_Time = ros::Time::now().toSec(); //get current time.
+            curr_Time_a = (adouble) curr_Time;
+            curr_Time_ref = curr_Time_a;
+            stamp_now = position_stamped; // define t_i
+            stamp_next = stamp_now + 0.2; // define t_(i+1)
+
+            rk4_control(0,0) = last_control; rk4_control(0,1) = last_control;
+            rk4_time(0,0) = stamp_now; rk4_time(0,1) = stamp_next;
+            init_states(0,0) = position;
+            init_states(1,0) = velocity; 
+            init_states(2,0) = FmassEst;
+
+            //void(& daee)() = dae;
+            
+            // RK4 propagate to get next states extimate. rk4_states
+            rk4_propagate(dae, rk4_control, rk4_time, 
+                    init_states, rk4_param, problem, 1, rk4_states, NULL);
+
+            // Generate initial guess states with RK4
+            int nnodes = problem.phases(1).nodes(0);
+            MatrixXd x0(3,nnodes);
+            MatrixXd init_state_guess(3,1);
+            init_state_guess(0,0) = rk4_states(0,1);
+            init_state_guess(1,0) = rk4_states(1,1);
+            init_state_guess(2,0) = rk4_states(2,1);
+            MatrixXd& init_state_guess_ref = init_state_guess;
+
+            problem.phases(1).guess.controls = last_control*ones(1,nnodes);
+            problem.phases(1).guess.time = linspace(stamp_next, stamp_next+2.5, nnodes);
+            rk4_propagate(dae, problem.phases(1).guess.controls,
+                problem.phases(1).guess.time, init_state_guess_ref, rk4_param,
+                problem, 1, x0, NULL);
+            
+            problem.phases(1).guess.states = x0;
+
+            ////////// problem bounds iinformation //////////
+            problem.phases(1).bounds.lower.states << 0, -2, 0;
+            problem.phases(1).bounds.upper.states << 3, 2, MF_INIT;
+
+            problem.phases(1).bounds.lower.controls << -1.0;
+            problem.phases(1).bounds.upper.controls << 1.0;
+
+            problem.phases(1).bounds.lower.events << position, velocity, FmassEst;//, 1.4, 0;
+            problem.phases(1).bounds.upper.events << position, velocity, FmassEst;//, 1.6, 0;
+
+            problem.phases(1).bounds.lower.StartTime = stamp_next; 
+            problem.phases(1).bounds.upper.StartTime = stamp_next;
+
+            problem.phases(1).bounds.lower.EndTime = stamp_next+2.5;
+            problem.phases(1).bounds.upper.EndTime = stamp_next+2.5;
+
+            ////////// Call PSOPT to solve the problem //////////
+            psopt(solution, problem, algorithm);
+            if(solution.error_flag) exit(0);
+
+            ////////// Create control signal //////////
+            MatrixXd uStar = solution.get_controls_in_phase(1);
+            MatrixXd t_sol = solution.get_time_in_phase(1);
+            next_control = uStar(0);
+
+            // make a schidmit trigger to transform continuous command to on-off command
+        
+
+            if (last_control == 1)
+            {
+                if(next_control.value() >= (1-KAPPA)*0.5) next_control.setValue(1);
+                else if(next_control.value() <= (1+KAPPA)*-0.5) next_control.setValue(-1);
+                else next_control.setValue(0);
+            }
+            else if (last_control == 0)
+            {
+                if(next_control.value() >= (1+KAPPA)*0.5) next_control.setValue(1);
+                else if(next_control.value() <= (1+KAPPA)*-0.5) next_control.setValue(-1);
+                else next_control.setValue(0);
+            }
+            else if (last_control == -1)
+            {
+                if(next_control.value() >= (1+KAPPA)*0.5) next_control.setValue(1);
+                else if(next_control.value() <= (1-KAPPA)*-0.5) next_control.setValue(-1);
+                else next_control.setValue(0);
+            }
+            else{ printf("last control error!"); exit(0);}
+
+            control.data = next_control.value();
+
+            // create a timer to publish next_control at stamp_next.
+            double time_now = ros::Time::now().toSec();
+            ros::Timer timeToPub =
+                Control_nh->createTimer(ros::Duration(stamp_next-time_now),
+                PubControlCb, true);
+
+            last_control = next_control.value();
+
+
+
+
+
+        }
+
+        void PubControlCb()
+        {
+            control_pub.publish(control);
+
+        }
+
+    private:
+        ros::Publisher control_pub;
+        float last_control;
+        adouble next_control;
+        
+
+
+};
 
 ///////////////////////////////////////
 ////////// PSOPT Config Part //////////
@@ -81,15 +241,12 @@ void events(adouble* e, adouble* initial_states, adouble* final_states,
     adouble x0 = initial_states[0];
     adouble v0 = initial_states[1];
     adouble m0 = initial_states[2];
-    //adouble xf = final_states[0];
-    //adouble vf = final_states[1];
+    
 
     e[0] = x0;
     e[1] = v0;
     e[2] = m0;
-    //e[3] = xf;
-    //e[4] = vf;
-    //TODO cancel final state constraint?;
+    
 }
 
 void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
@@ -104,7 +261,7 @@ double position = 0.0;
 double position_stamped;
 double velocity = 0.0;
 double velocity_stamped;
-double FmassEst = 0.5;
+double FmassEst = MF_INIT;
 
 std_msgs::Float32 control; // on-off control command
 std_msgs::Float32 cont_control; //continuous control command
@@ -126,38 +283,13 @@ void massCb(const std_msgs::Float32::ConstPtr& msg)
     FmassEst = msg->data;
 }
 
+
 ////////////////////////////////
 ///////// Main Routine /////////
 ////////////////////////////////
 
 int main(int argc, char **argv)
 {
-    ////////// ROS Setup //////////
-    ros::init(argc, argv, "one_dim_ocp_node");
-    ros::NodeHandle nh;
-
-    // subcribe height, velocity and mass estimate
-    ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::PoseStamped>
-        ("height", 1, poseCb);
-
-    ros::Subscriber velocity_sub = nh.subscribe<geometry_msgs::TwistStamped>
-        ("velocity", 1, velocityCb);
-
-    ros::Subscriber mass_sub = nh.subscribe<std_msgs::Float32>
-        ("mass_est",1 , massCb);
-
-    //Publish control (on-off)
-    ros::Publisher control_pub = nh.advertise<std_msgs::Float32>
-        ("control", 1);
-
-    // Publish the continuous control
-    ros::Publisher cont_control_pub = nh.advertise<std_msgs::Float32>
-        ("cont_control",1);
-
-
-    // set the control publish rate at 5Hz.
-    ros::Rate rate(5);
-
     ////////// PSOPT Setup //////////
     // declare key structures
     Alg algorithm;
@@ -181,23 +313,7 @@ int main(int argc, char **argv)
     problem.phases(1).nodes <<10;
     psopt_level2_setup(problem, algorithm);
 
-    /*********** Move into ROS while loop **************
-    // problem bounds information
-    problem.phases(1).bounds.lower.states << 0, 0, 0;
-    problem.phases(1).bounds.upper.states << 3, 2, MF_INIT;
-
-    problem.phases(1).bounds.lower.controls << -1.0;
-    problem.phases(1).bounds.upper.controls << 1.0;
-
-    problem.phases(1).bounds.lower.events << 0, 0, MF_INIT;//, 1.4, 0;
-    problem.phases(1).bounds.upper.events << 0, 0, MF_INIT;//, 1.6, 0;
-
-    problem.phases(1).bounds.lower.StartTime = 0.0;
-    problem.phases(1).bounds.upper.StartTime = 0.0;
-
-    problem.phases(1).bounds.lower.EndTime = 0.0;
-    problem.phases(1).bounds.upper.EndTime = 0.5;
-    *******************************************************/
+    
 
     ////////// Rigister problem functions //////////
 
@@ -217,121 +333,40 @@ int main(int argc, char **argv)
     algorithm.collocation_method = "Legendre";
 
     //Intialize the last control signal
-    float last_control = 0.0;
-    ////////// The ROS While Loop //////////
-    while(ros::ok())
-    {
-        // Get current time 
-        double curr_Time = ros::Time::now().toSec();
-        adouble curr_Time_a;
-        curr_Time_a = (adouble) curr_Time;
-        adouble& curr_Time_ref = curr_Time_a; // reference of current time.
-        
-        if (curr_Time < position_stamped)
-        {
-            printf("Time stamp error!");
-            exit(0);
-        }
+    //float last_control = 0.0;
 
-        MatrixXd rk4_control(1,2);
-        rk4_control(0,0) = last_control; rk4_control(0,1) = last_control;
-        MatrixXd rk4_time(1,2);
-        rk4_time(0,0) = position_stamped; rk4_time(0,1) = curr_Time;
-        MatrixXd rk4_state(3,2);
-        MatrixXd rk4_param;
-        MatrixXd init_states(3,1);
-        init_states(0,0) = position; init_states(1,0) = velocity; init_states(2,0) = FmassEst;
-        // RK4 propagate from sample to current time
-        rk4_propagate(&dae, rk4_control, rk4_time, init_states, rk4_param, problem,1,rk4_state, NULL);
-                       
+    ////////// ROS Setup //////////
+    ros::init(argc, argv, "one_dim_ocp_node");
+    ros::NodeHandle nh;
 
-        // generate the propagation control and time sequence.
-        // use last control signal as control trajectory.
-        // the 
+    // Create AsyncSpinner
+    ros::AsyncSpinner spinner(0);
+    spinner.start();
 
-        
-        ////////// Initial guess ////////// 
-        int nnodes= problem.phases(1).nodes(0);
+    // subcribe height, velocity and mass estimate
+    ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::PoseStamped>
+        ("height", 1, poseCb);
 
-        // Use RK4 Propagation to generate initial guess.
+    ros::Subscriber velocity_sub = nh.subscribe<geometry_msgs::TwistStamped>
+        ("velocity", 1, velocityCb);
 
-        MatrixXd x0(3,nnodes);
-        MatrixXd init_state_guess(3,1);
-        init_state_guess(0,0) = rk4_state(0,1);
-        init_state_guess(1,0) = rk4_state(1,1);
-        init_state_guess(2,0) = rk4_state(2,1);
+    ros::Subscriber mass_sub = nh.subscribe<std_msgs::Float32>
+        ("mass_est",1 , massCb);
 
-        MatrixXd& init_state_guess_ref = init_state_guess;
+    // Publish the continuous control
+    ros::Publisher cont_control_pub = nh.advertise<std_msgs::Float32>
+        ("cont_control",1);
 
-        problem.phases(1).guess.controls       = last_control*ones(1,nnodes);
-        problem.phases(1).guess.time           = linspace(curr_Time, curr_Time+2.5, nnodes);
-        rk4_propagate(&dae, problem.phases(1).guess.controls, problem.phases(1).guess.time,
-                        init_state_guess_ref, rk4_param, problem, 1, x0, NULL);
-        problem.phases(1).guess.states         = x0;
-        // problem bounds information
-        problem.phases(1).bounds.lower.states << 0, -2, 0;
-        problem.phases(1).bounds.upper.states << 3, 2, MF_INIT;
+    // create an instance of MainControlLoop
+    MainControlLoop main_control_loop(&nh, problem, solution, algorithm);
 
-        problem.phases(1).bounds.lower.controls << -1.0;
-        problem.phases(1).bounds.upper.controls << 1.0;
+    ////////// Setup control loop timer ////
+    ros::Timer controlPubTimer = nh.createTimer
+        (ros::Duration(1.0/5.0),
+        &MainControlLoop::GetNextControl, &main_control_loop);    
+    
 
-        problem.phases(1).bounds.lower.events << position, velocity, FmassEst;//, 1.4, 0;
-        problem.phases(1).bounds.upper.events << position, velocity, FmassEst;//, 1.6, 0;
-
-        problem.phases(1).bounds.lower.StartTime = position_stamped; 
-        problem.phases(1).bounds.upper.StartTime = position_stamped;
-
-        problem.phases(1).bounds.lower.EndTime = position_stamped+2.5;
-        problem.phases(1).bounds.upper.EndTime = position_stamped+2.5;
-
-        ////////// Call PSOPT to solve the problem //////////
-        psopt(solution, problem, algorithm);
-        if (solution.error_flag) exit(0);
-
-        ////////// Create control signal //////////
-        MatrixXd uStar = solution.get_controls_in_phase(1);
-        MatrixXd t_sol = solution.get_time_in_phase(1);
-        // interpolate control at current time
-        adouble control_now;
-        //spline_interpolation(&control_now, curr_Time_ref, t_sol, uStar, nnodes);
-
-        control_now = uStar(0);
-        printf("control now value %f \n", control_now.value());
-
-        // publish the continuous control data
-        cont_control.data = control_now.value();
-        cont_control_pub.publish(cont_control);
-
-        // make a schidmit trigger to transform continuous command to on-off command
-        float kappa = 0.2;
-
-       if (last_control == 1)
-       {
-           if(control_now.value() >= (1-kappa)*0.5) control_now.setValue(1);
-           else if(control_now.value() <= (1+kappa)*-0.5) control_now.setValue(-1);
-           else control_now.setValue(0);
-       }
-       else if (last_control == 0)
-       {
-           if(control_now.value() >= (1+kappa)*0.5) control_now.setValue(1);
-           else if(control_now.value() <= (1+kappa)*-0.5) control_now.setValue(-1);
-           else control_now.setValue(0);
-       }
-       else if (last_control == -1)
-       {
-           if(control_now.value() >= (1+kappa)*0.5) control_now.setValue(1);
-           else if(control_now.value() <= (1-kappa)*-0.5) control_now.setValue(-1);
-           else control_now.setValue(0);
-       }
-       else{ printf("last control error!"); exit(0);}
-
-        control.data = control_now.value();
-        control_pub.publish(control);
-        // store control value for next propagation
-        last_control = control_now.value();
-
-        ros::spinOnce();
-        rate.sleep();      
+    ros::waitForShutdown();   
 
 
     }
