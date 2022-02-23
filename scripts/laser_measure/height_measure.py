801d21a910b2f1d20d590a36add7993da73faa16
Ubuntu <ubuntu@ubuntu.mshome.net>
2022-02-23 18:19:42 +0800

    publish height with time stamped @20Hz

--- scripts/laser_measure/height_measure.py
+++ scripts/laser_measure/height_measure.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Float32
+from geometry_msgs.msg import PointStamped
 from smbus2 import SMBus
 import time
 import numpy as np
@@ -12,31 +14,39 @@ reg_cmd = 0x00
 reg_stat = 0x01
 reg_meas_low = 0x10
 reg_meas_high = 0x11
+reg_accuracy = 0xEB
 
-bus = SMBUS(i2c_channel)
+bus = SMBus(i2c_channel)
 
 def get_distance():
-	pub = rospy.Publisher('height', Float32, queue_size=1)
+	pub = rospy.Publisher('height', PointStamped , queue_size=1)
 	rospy.init_node('laser_measure', anonymous=True)
 	rate = rospy.Rate(20)  # Publish distance at 20Hz
-	# initialize measurement
+	# Lidar adjust accuracy & speed
+	bus.write_byte_data(address, reg_accuracy, 0x0B)
+	# initialize measurement3
 	measure = np.float16(0.0)
+	height_data = PointStamped()
 
 	while not rospy.is_shutdown():
 		# Start Garmin Lidar Lite v4 measure procedure
 		# 1. Write 0x04 to reg 0x00
-		bus.write_i2c_block_data(address, reg_cmd, 0x04)
+		bus.write_byte_data(address, reg_cmd, 0x04)
 		# 2. Read reg 0x01 (Repeat until LSB goes low)
-		while (bus.read_i2c_block_data(address, reg_stat, 1) & 0x01):
+		while (bus.read_byte_data(address, reg_stat, 1) & 0x01):
 			pass
 		result = bus.read_i2c_block_data(address,reg_meas_low, 2)
 		measure = ((result[1] & 0xFF) << 8) | (result[0] & 0xFF)
 		#convert measure data from float16 to float32
-		measure32 = np.float32(measure)
-		pub.publish(measure32)  # publish data
+		measure64 = np.float64(measure)
+		print(measure64)
+
+		height_data.header.stamp = rospy.get_rostime()
+		height_data.point.z = measure64
+		pub.publish(height_data)  # publish data
 		rate.sleep()
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 	try:
 		get_distance()
 	except rospy.ROSInterruptException:
